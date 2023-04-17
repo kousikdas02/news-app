0534ec65015ab2a525bc00421b4f0b730228b115
kousikdas <kousik.das@webskitters.com>
2023-04-17 12:17:30 +0530

    search functionality

--- src/pages/NewsTrending/NewsTrending.jsx
+++ src/pages/NewsTrending/NewsTrending.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Button, Container, Row } from 'react-bootstrap';
+import ReactLoading from "react-loading";
 
 const NewsTrending = () => {
   const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
   const BaseUrl =
-    "https://newsapi.org/v2/top-headlines?country=in&apiKey=5bef830707fa4e92b4f4c390fb95dde7";
+    "https://newsapi.org/v2/top-headlines?country=in&apiKey=f2b86ca3d3ac43d689830404d4545385";
   const getNewsData = async () => {
     try {
       const response = await axios.get(BaseUrl);
@@ -13,6 +15,10 @@ const NewsTrending = () => {
     } catch (error) {
       console.log("error", error);
     }
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    
   };
 
   useEffect(() => {
@@ -27,54 +33,92 @@ const NewsTrending = () => {
   };
   return (
     <>
-      <Container>
-        <Row>
-          {news?.slice(0, loadMoreNews)?.map((newsData, index) => {
-            const [date, time] = newsData.publishedAt.split("T")
-            const placeholderImage = "http://via.placeholder.com/640x360"
-            return (
-              <>
-                <div className="col-md-4 mt-3" key={index}>
-                  <div className="card">
-                    <div className="card-wrapper">
-                      <img
-                        src={newsData.urlToImage ? newsData.urlToImage : placeholderImage}
-                        className="card-img-top img-fluid"
-                        alt="..."
-                      />
-                    </div>
-                    <div className="card-body">
-                      <div className=" cardInfo">
-                        <p className="text-muted authorName">Author: {newsData.author ?  newsData.author : "Unknown"}</p>
-                        <p className="text-muted">Date:{date}</p>
-                        <p className="text-muted">Time:{time}</p>
-                      </div>
-                      <h5 className="card-title">
-                        {newsData.title.slice(0, 35)}...
-                      </h5>
-                      <p className="card-text">
-                        {newsData.description &&
-                          newsData.description.slice(0, 55)}
-                        ...
-                      </p>
-                      <a href={newsData.url} className="btn btn-primary">
-                        READ MORE...
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </>
-            );
-          })}
-          {
-            loadMoreNews < news.length && (
-              <Button className='mt-4 loadMore_btn' onClick={handleMoreImage}>
-                                Load more
-                            </Button>
-            )
-          }
-        </Row>
-      </Container>
+      {loading === true ? (
+        <ReactLoading className="loader" type={"spin"} color={"black"} width={'100px'}/>
+      ) : (
+        <>
+          <div className='commonWpr'>
+            <Container>
+              <Row>
+                {news
+                  ?.slice(0, loadMoreNews)
+                  ?.map((newsData, index) => {
+                    const [date, time] =
+                      newsData.publishedAt.split("T");
+                    const placeholderImage =
+                      "http://via.placeholder.com/640x360";
+                    return (
+                      <>
+                        <div
+                          className='col-md-4 mt-3'
+                          key={index}>
+                          <div className='card'>
+                            <div className='card-wrapper'>
+                              <img
+                                src={
+                                  newsData.urlToImage
+                                    ? newsData.urlToImage
+                                    : placeholderImage
+                                }
+                                className='card-img-top img-fluid'
+                                alt='...'
+                              />
+                            </div>
+                            <div className='card-body'>
+                              <div className=' cardInfo'>
+                                <p className='text-muted authorName'>
+                                  Author:{" "}
+                                  {newsData.author
+                                    ? newsData.author
+                                    : "Unknown"}
+                                </p>
+                                <p className='text-muted'>
+                                  Date:{date}
+                                </p>
+                                <p className='text-muted'>
+                                  Time:{time}
+                                </p>
+                              </div>
+                              <h5 className='card-title'>
+                                {newsData.title.slice(
+                                  0,
+                                  35
+                                )}
+                                ...
+                              </h5>
+                              <p className='card-text'>
+                                {newsData.description &&
+                                  newsData.description.slice(
+                                    0,
+                                    55
+                                  )}
+                                ...
+                              </p>
+                              <a
+                                href={
+                                  newsData.url
+                                }
+                                className='btn btn-primary'>
+                                READ MORE...
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    );
+                  })}
+                {loadMoreNews < news.length && (
+                  <Button
+                    className='mt-4 loadMore_btn'
+                    onClick={handleMoreImage}>
+                    Load more
+                  </Button>
+                )}
+              </Row>
+            </Container>
+          </div>
+        </>
+      )}
     </>
   )
 }
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import "./SearchPage.scss"
+import axios from "axios";
 
 const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
-}
+  const [searchNews, setSearchNews] = useState([]);
+  const [inputData, setInputData] = useState('');
 
-export default SearchPage
+  const search = async (e) => {
+    const query = e.target.value;
+    setInputData(query)
+    const response = await axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=f2b86ca3d3ac43d689830404d4545385`)
+    setSearchNews(response.data.articles)
+  }
+  useEffect(() => {
+    search()
+  }, []);
+
+  console.log(searchNews)
+  const newsPerRow = 9;
+  const [loadMoreNews, setloadMoreNews] = useState(newsPerRow);
+  const handleMoreImage = () => {
+    setloadMoreNews(loadMoreNews + newsPerRow);
+  };
+
+  return (
+    <>
+      <div className='commonWpr'>
+        <Container>
+          <h1>Search News</h1>
+          <div className='searchInput'>
+            <input type='text' placeholder='Search News' onChange={search} />
+          </div>
+
+          <div className="showSearchText">
+            {
+              inputData.length > 0 &&
+              <>
+                <h3>You Searched : {inputData}</h3>
+              </>
+            }
+          </div>
+
+          <Row>
+            {searchNews.length > 0 && searchNews.slice(0, loadMoreNews).map((newsData, index) => {
+              const [date, time] =
+                newsData.publishedAt.split("T");
+              const placeholderImage =
+                "http://via.placeholder.com/640x360";
+              return (
+                <>
+                  <div
+                    className='col-md-4 mt-3'
+                    key={index}>
+                    <div className='card'>
+                      <div className='card-wrapper'>
+                        <img
+                          src={
+                            newsData.urlToImage
+                              ? newsData.urlToImage
+                              : placeholderImage
+                          }
+                          className='card-img-top img-fluid'
+                          alt='...'
+                        />
+                      </div>
+                      <div className='card-body'>
+                        <div className=' cardInfo'>
+                          <p className='text-muted authorName'>
+                            Author:{" "}
+                            {newsData.author
+                              ? newsData.author
+                              : "Unknown"}
+                          </p>
+                          <p className='text-muted'>
+                            Date:{date}
+                          </p>
+                          <p className='text-muted'>
+                            Time:{time}
+                          </p>
+                        </div>
+                        <h5 className='card-title'>
+                          {newsData.title.slice(
+                            0,
+                            35
+                          )}
+                          ...
+                        </h5>
+                        <p className='card-text'>
+                          {newsData.description &&
+                            newsData.description.slice(
+                              0,
+                              55
+                            )}
+                          ...
+                        </p>
+                        <a
+                          href={
+                            newsData.url
+                          }
+                          className='btn btn-primary'>
+                          READ MORE...
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+            {loadMoreNews < searchNews.length && (
+                  <Button
+                    className='mt-4 loadMore_btn'
+                    onClick={handleMoreImage}>
+                    Load more
+                  </Button>
+                )}
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default SearchPage;
